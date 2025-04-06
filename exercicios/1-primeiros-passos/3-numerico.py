f5e2d5eac26d6f994f0b6bbdb26e6a7187fbc791
Levi Bastos <levibastos@gmail.com>
2025-04-06 16:26:37 +0000

    Operadores com string

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,19 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+nome = 'Levi'
+nome = nome * 3
+print(nome)
+print(f"Levi estuda {3-1} horas por dia para aprender Python")
 
 # Realize uma operação entre dados do tipo inteiro e float
+soma = 30/8
+print(soma)
+print(type(soma))
+print(int(soma))
+print(round(soma,0))
