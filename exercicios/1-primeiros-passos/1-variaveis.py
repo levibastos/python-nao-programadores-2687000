86ad265dc2bcd075e9ec27a323de37f2449a91ce
Levi Bastos <levibastos@gmail.com>
2025-04-06 05:23:07 +0000

    Exercicio2

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,16 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = "Levi"
+sobrenome = "Bastos"
+print (nome, sobrenome)
 
 # Declare variáveis de cada tipo numérico
-
+num_int = 2
+num_float = 34.12
+print('Numero Inteiro:', num_int, 'Numero Float:', num_float)
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
+print ('Se e', verdadeiro, ', nao e falso', falso)
